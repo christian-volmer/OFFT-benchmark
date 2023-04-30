7b5e50a924e50c1be2b14a6fb1454ce9e70d9eef
Christian Volmer <117482707+christian-volmer@users.noreply.github.com>
2023-04-30 18:31:36 +0200

    Adding copyright notice

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 
+//          Copyright Christian Volmer 2023.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #define USE_MKL
 #define USE_FFTW
 
